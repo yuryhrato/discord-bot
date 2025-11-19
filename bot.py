87939a890b43283d331e64e8ee613ac2d9772e25
yuryhrato <50268526+yuryhrato@users.noreply.github.com>
2025-11-19 00:53:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,28 +5,36 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("TOKEN")
+
+CANAL_TRADUCAO = 1440515390072557618  # coloque aqui o ID do canal que deve traduzir
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f"Bot conectado como {bot.user}")
+
 
 @bot.listen("on_message")
 async def auto_translate(message):
     if message.author.bot:
         return
 
-    text = message.content
+    # se estiver fora do canal escolhido, ignora
+    if message.channel.id != CANAL_TRADUCAO:
+        return
+
+    texto = message.content
 
     try:
         traducoes = {
-            "pt": GoogleTranslator(source="auto", target="pt").translate(text),
-            "en": GoogleTranslator(source="auto", target="en").translate(text),
-            "ru": GoogleTranslator(source="auto", target="ru").translate(text),
+            "pt": GoogleTranslator(source="auto", target="pt").translate(texto),
+            "en": GoogleTranslator(source="auto", target="en").translate(texto),
+            "ru": GoogleTranslator(source="auto", target="ru").translate(texto)
         }
 
         resposta = (
@@ -39,5 +47,6 @@ async def auto_translate(message):
 
     except Exception as e:
         print("Erro:", e)
+
 
 bot.run(TOKEN)
